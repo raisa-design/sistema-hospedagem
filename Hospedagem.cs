3b4beebe398c23426e12de4d57c6012a52f9a5e5
Raisa Almeida <almeida.alves.raisa@gmail.com>
2023-05-23 21:25:25 -0300

    primeiro commit

---
+++ Hospedagem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Pessoa
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+}
+
+class Suíte
+{
+    public int Número { get; set; }
+    public decimal ValorDiária { get; set; }
+}
+
+class Reserva
+{
+    public Pessoa Hóspede { get; set; }
+    public Suíte Suíte { get; set; }
+    public DateTime DataInício { get; set; }
+    public DateTime DataFim { get; set; }
+
+    public decimal CalcularValorTotal()
+    {
+        TimeSpan duraçãoReserva = DataFim - DataInício;
+        int diasReserva = duraçãoReserva.Days + 1;
+        decimal valorTotal = Suíte.ValorDiária * diasReserva;
+
+        if (diasReserva > 10)
+        {
+            valorTotal *= 0.9m; 
+        }
+
+        return valorTotal;
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Pessoa hóspede = new Pessoa { Nome = "João", Idade = 30 };
+        Suíte suíte101 = new Suíte { Número = 101, ValorDiária = 150 };
+        Reserva reserva = new Reserva
+        {
+            Hóspede = hóspede,
+            Suíte = suíte101,
+            DataInício = new DateTime(2023, 5, 1),
+            DataFim = new DateTime(2023, 5, 15)
+        };
+
+        
+        decimal valorTotal = reserva.CalcularValorTotal();
+
+   
+        Console.WriteLine("Dados do Hóspede:");
+        Console.WriteLine("Nome: " + reserva.Hóspede.Nome);
+        Console.WriteLine("Idade: " + reserva.Hóspede.Idade);
+        Console.WriteLine("\nDados da Reserva:");
+        Console.WriteLine("Suíte: " + reserva.Suíte.Número);
+        Console.WriteLine("Data de Início: " + reserva.DataInício.ToString("dd/MM/yyyy"));
+        Console.WriteLine("Data de Fim: " + reserva.DataFim.ToString("dd/MM/yyyy"));
+        Console.WriteLine("Valor Total da Reserva: " + valorTotal.ToString("C"));
+    }
+}
